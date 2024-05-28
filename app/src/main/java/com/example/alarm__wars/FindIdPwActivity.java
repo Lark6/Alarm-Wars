40a5710cfd644d7f036cb30eec970c4791bbb434
BlueSky8bya <blackspace665@gmail.com>
2024-05-28 15:00:59 +0900

    업데이트 내용 xml 파일들 가독성 개선
    남은 과제:
    1. firebase와 고백 DB 연동하기
    2. firebase api 이용해서 SMS 서비스를 통한 인증번호 받기
    3. 로그인창의 간편 로그인 구글, 네이버, 카카오 API 이용해서 로그인 해결하기

---
+++ app/src/main/java/com/example/alarm__wars/FindIdPwActivity.java
@@ -0,0 +1,67 @@
+package com.example.alarm__wars;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class FindIdPwActivity extends AppCompatActivity {
+
+    private TextView tabId;
+    private TextView tabPw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findidpw);
+
+        tabId = findViewById(R.id.tab_id);
+        tabPw = findViewById(R.id.tab_pw);
+
+        tabId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectTab(true);
+            }
+        });
+
+        tabPw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectTab(false);
+            }
+        });
+
+        // Initialize with ID tab selected
+        selectTab(true);
+    }
+
+    private void selectTab(boolean isIdTabSelected) {
+        if (isIdTabSelected) {
+            loadFragment(new IdFragment());
+            tabId.setBackgroundResource(R.drawable.tab_background_selected);
+            tabId.setTextColor(getResources().getColor(R.color.white));
+            tabPw.setBackgroundResource(R.drawable.tab_background);
+            tabPw.setTextColor(getResources().getColor(R.color.darkCharcoal));
+        } else {
+            loadFragment(new PwFragment());
+            tabPw.setBackgroundResource(R.drawable.tab_background_selected);
+            tabPw.setTextColor(getResources().getColor(R.color.white));
+            tabId.setBackgroundResource(R.drawable.tab_background);
+            tabId.setTextColor(getResources().getColor(R.color.darkCharcoal));
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/alarm__wars/PwFragment.java
@@ -0,0 +1,32 @@
+package com.example.alarm__wars;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PwFragment extends Fragment {
+
+    private Spinner phoneSpinner;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pw, container, false);
+
+        phoneSpinner = view.findViewById(R.id.phone_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.phone_prefix, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        phoneSpinner.setAdapter(adapter);
+        phoneSpinner.setSelection(0);  // Set the default selection to "010"
+
+        return view;
+    }
+}
